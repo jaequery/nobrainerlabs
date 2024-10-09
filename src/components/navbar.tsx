69371b09e0e1e4dd301755f29ccbe3e65da54a4c
Jae Lee <jaequery@gmail.com>
2024-10-08 22:58:18 -0700

    update nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -13,7 +13,7 @@ import { PlusGrid, PlusGridItem, PlusGridRow } from './plus-grid'
 
 const links = [
   // { href: '/pricing', label: 'Pricing' },
-  { href: '/company', label: 'Company' },
+  { href: '/company', label: 'Our Mission' },
   // { href: '/blog', label: 'Blog' },
   // { href: '/login', label: 'Login' },
 ]
