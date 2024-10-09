01a246e66a3fd25ae86b6145eb4403c1e95df4d9
Jae Lee <jaequery@gmail.com>
2024-10-09 07:21:18 -0700

    update copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ function Hero() {
         />
         <div className="pb-24 pt-16 sm:pb-32 sm:pt-24 md:pb-48 md:pt-32">
           <h1 className="font-display text-balance text-6xl/[0.9] font-medium tracking-tight text-gray-950 sm:text-8xl/[0.8] md:text-9xl/[0.8]">
-            Build your startup with us.
+            Build your next startup with us.
           </h1>
           <p className="mt-8 max-w-lg text-xl/7 font-medium text-gray-950/75 sm:text-2xl/8">
             We&apos;re your partner for startup success, from idea to growth.
